3a60f9d04a4a47aa31695643f0574c4f15dd750b
Artyom Pratsko <kabanproff@gmail.com>
2025-06-23 17:55:51 +0300

    edit path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  app: {
+    baseURL: '/test-scamacca/'
+  },
   css: [
   //   '@/assets/css/variables.css',
     '@/assets/scss/main.scss',
